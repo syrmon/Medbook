c9b9f7ec8b3fce5c5e4399e4f5e644a601ef85d5
syrmon <mmehemmsd@gmail.com>
2023-10-08 12:40:06 +0400

    vreated search bar

--- frontend/src/components/banner/Banner.jsx
+++ frontend/src/components/banner/Banner.jsx
@@ -1,15 +1,34 @@
-import { Person } from "@mui/icons-material";
+import { Person, Search } from "@mui/icons-material";
 import styles from "./styles.module.css";
+import { IconButton, Input, InputAdornment, TextField } from "@mui/material";
+import { useState } from "react";
 
 const Banner = () => {
+  const [searchValue, setSearchValue] = useState();
+
   return (
     <div className={`${styles.container}`}>
       <h2 className={`${styles.productName}`}>Medibook</h2>
-      <div className={`${styles.personOutline}`}>
-        <Person
-          sx={{ fontSize: 30 }}
-          style={{ color: "#fff"}}
+      <div className={`${styles.searchContainer}`}>
+        <TextField
+          className={`${styles.searchInput}`}
+          variant="standard"
+          placeholder="Axtar"
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+            disableUnderline: true,
+          }}
         />
+      </div>
+      <div className={`${styles.personOutline}`}>
+        <Person sx={{ fontSize: 30 }} style={{ color: "#fff" }} />
       </div>
     </div>
   );
