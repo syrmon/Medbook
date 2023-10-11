047d3ff338ffdbaff697a9a15a64c8d00fe9f5dc
syrmon <mmehemmsd@gmail.com>
2023-10-11 16:48:22 +0400

    edited view

--- frontend/src/components/receipeBox/ReceipeBox.jsx
+++ frontend/src/components/receipeBox/ReceipeBox.jsx
@@ -11,17 +11,20 @@ const ReceipeBox = (props) => {
         </h2>
       </div>
       <div className={`${styles.info}`}>
-        <p className={`${styles.desc}`}>{data.workDescription}</p>
+        <p className={`${styles.desc}`}>{data.workDescription} ve sair elave xercler oldu tutaqki onda nece gorsenecek</p>
         <h4 className={`${styles.amount}`}>{data.amount} AZN</h4>
       </div>
       <div className={`${styles.info}`}>
         <p className={`${styles.desc}`}>{data.outcomeDetails}</p>
-        <h4 className={`${styles.amount}`}>{data.outcome} AZN</h4>
+        <h4 className={`${styles.outcome}`}>-{data.outcome} AZN</h4>
       </div>
       <div className={`${styles.info}`}>
         <p className={`${styles.date}`}>
           {data.date} {data.time}
         </p>
+        <h4 >
+          Qazanc: <span className={`${styles.income}`}> {Number(data.amount) - Number(data.outcome)} AZN</span>
+        </h4>
       </div>
     </div>
   );
